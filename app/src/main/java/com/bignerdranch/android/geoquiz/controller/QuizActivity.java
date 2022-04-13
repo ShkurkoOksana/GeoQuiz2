6c630875122603551140055a79ab2b9ca2e6a7be
a16708387 <al#&5mPKs11>
2022-04-13 19:53:54 +0700

    Add Previous button and logic for get previous question

--- app/src/main/java/com/bignerdranch/android/geoquiz/controller/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/controller/QuizActivity.java
@@ -6,26 +6,34 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bignerdranch.android.geoquiz.R;
+import com.bignerdranch.android.geoquiz.model.QuizModelImpl;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private Button mPrevButton;
+    private TextView mQuestionTextView;
+
+    private QuizModelImpl mQuizModel = new QuizModelImpl();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion(mQuizModel.getQuestionResId());
+
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 500);
-                toast.show();
+                checkAnswer(true);
             }
         });
 
@@ -33,10 +41,41 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 500);
-                toast.show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(mQuizModel.getNextQuestionResId());
+            }
+        });
+
+        mPrevButton = findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(mQuizModel.getPrevQuestionResId());
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuizModel.getTrueAnswer();
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion(int questionResId) {
+        mQuestionTextView.setText(questionResId);
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/model/QuizModelImpl.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/model/QuizModelImpl.java
@@ -1,4 +1,39 @@
 package com.bignerdranch.android.geoquiz.model;
 
+import com.bignerdranch.android.geoquiz.R;
+
 public class QuizModelImpl {
+    Question[] mQuestions = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_america, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    public int getQuestionResId() {
+        return mQuestions[mCurrentIndex].getTextResId();
+    }
+
+    public int getNextQuestionResId() {
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestions.length;
+        return mQuestions[mCurrentIndex].getTextResId();
+    }
+
+    public int getPrevQuestionResId() {
+        if (mCurrentIndex == 0) {
+            mCurrentIndex = mQuestions.length - 1;
+        } else {
+            mCurrentIndex = (mCurrentIndex - 1) % mQuestions.length;
+        }
+
+        return mQuestions[mCurrentIndex].getTextResId();
+    }
+
+    public boolean getTrueAnswer() {
+        return mQuestions[mCurrentIndex].isAnswerTrue();
+    }
 }
